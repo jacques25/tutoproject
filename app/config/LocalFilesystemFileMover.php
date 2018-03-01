c5e6f3b4a69aafc62815d5c657de3acd29ff34fb
jacques25 <jacques19611@live.fr>
2018-03-01 18:02:40 +0100

    add file in AppBundleService

---
+++ app/config/LocalFilesystemFileMover.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Service;
+
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class LocalFilesystemFileMover implements FileMoverI
+{
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    public function move($existingFilePath, $newFilePath)
+    {
+        $this->filesystem->rename($existingFilePath, $newFilePath);
+
+        return true;
+    }
+}
